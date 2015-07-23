a1607aa3c6ef908b5c625ba0d97b94462fa85eed
Maximilian Weber <weber.maximilian@googlemail.com>
2015-07-23 17:58:11 +0200

    First version of the blob storage protocol.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject sv/blob-storage "0.1.0-SNAPSHOT"
+  :description "A protocol for storing binary large objects."
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
---
+++ src/sv/blob_storage.clj
@@ -0,0 +1,19 @@
+(ns sv.blob-storage)
+
+(defprotocol BlobStorage
+  "A protocol for storing binary large objects. The protocol tries to
+   foster implementations, which treat blobs as immutable data."
+  (store
+    [this ^java.io.InputStream in]
+    "Stores the data from the InputStream in the BlobStorage and
+     returns an UUID that must be used to retrieve the data again.")
+  (retrieve
+    [this ^java.util.UUID id]
+    "Retrieves the data which is stored under the given UUID from the
+     BlobStorage. Returns a java.io.InputStream with the data or nil, if
+     a blob with the given UUID was not found.")
+  (delete
+    [this ^java.util.UUID id]
+    "Deletes the data which is stored under the given UUID from the
+     BlobStorage. Returns true, if the blob was found and deleted,
+     otherwise false."))
