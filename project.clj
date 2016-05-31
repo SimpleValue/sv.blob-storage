794da03fa664debc1684378bf51253eff5745039
Maximilian Weber <weber.maximilian@googlemail.com>
2016-05-31 17:45:33 +0200

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sv/blob-storage "0.1.0"
+(defproject sv/blob-storage "0.1.1-SNAPSHOT"
   :description "A protocol for storing binary large objects."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
