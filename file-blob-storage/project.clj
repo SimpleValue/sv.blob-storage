2b589bdc2380c8c5fb8bfd6eaae9cd743cbd2179
Maximilian Weber <weber.maximilian@googlemail.com>
2016-06-01 07:44:06 +0200

    Moved file-blob-storage to https://github.com/SimpleValue/sv.blob-storage.file-blob-storage

--- file-blob-storage/project.clj
+++
@@ -1,6 +0,0 @@
-(defproject sv.blob-storage/file-blob-storage "0.1.0-SNAPSHOT"
-  :description "A blob storage implementation that uses the file
-                system as storage."
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
